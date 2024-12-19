fcd4d12634d6c4e6e4adb9617b3fb25af768468e
BMorford <22627049+BMorford@users.noreply.github.com>
2024-12-19 10:06:33 -0700

    Init Add day 1 solution

---
+++ Day1/Day1.cs
@@ -0,0 +1,8 @@
+﻿IEnumerable<string> input = File.ReadLines("./Day1/input.txt");
+
+IEnumerable<string[]> sortedInput = input.Select(x => x.Split(new char[0], StringSplitOptions.RemoveEmptyEntries));
+(IOrderedEnumerable<string>, List<string>) lists = (sortedInput.Select(x => x[0]).Order(), sortedInput.Select(x => x[1]).Order().ToList());
+
+int sum = lists.Item1.Select((x, i) => new { value = int.Parse(x), i }).Sum(y => Math.Abs(y.value - int.Parse(lists.Item2[y.i])));
+
+Console.WriteLine(sum.ToString());
