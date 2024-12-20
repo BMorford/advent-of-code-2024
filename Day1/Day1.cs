7f58665d5662c3f81d9370f761f54306ceb9f8ed
BMorford <22627049+BMorford@users.noreply.github.com>
2024-12-20 12:00:24 -0700

    Add day 2 part 1 and 2 solutions Update day 1 variable names

--- Day1/Day1.cs
+++ Day1/Day1.cs
@@ -1,14 +1,14 @@
-﻿IEnumerable<string> input = File.ReadLines("./Day1/input.txt");
+﻿IEnumerable<string> input = File.ReadLines("./input.txt");
 
 // Part 1
-IEnumerable<string[]> sortedInput = input.Select(x => x.Split(new char[0], StringSplitOptions.RemoveEmptyEntries));
-(IOrderedEnumerable<string>, List<string>) lists = (sortedInput.Select(x => x[0]).Order(), sortedInput.Select(x => x[1]).Order().ToList());
+IEnumerable<string[]> splitInput = input.Select(x => x.Split(new char[0], StringSplitOptions.RemoveEmptyEntries));
+(IOrderedEnumerable<string>, List<string>) sortedLists = (splitInput.Select(x => x[0]).Order(), splitInput.Select(x => x[1]).Order().ToList());
 
-int sum = lists.Item1.Select((x, i) => new { value = int.Parse(x), i }).Sum(y => Math.Abs(y.value - int.Parse(lists.Item2[y.i])));
+int sum = sortedLists.Item1.Select((x, i) => new { value = int.Parse(x), i }).Sum(y => Math.Abs(y.value - int.Parse(sortedLists.Item2[y.i])));
 
 Console.WriteLine(sum.ToString());
 
 // Part 2
-int similarityScore = lists.Item1.Sum(x => int.Parse(x) * lists.Item2.Count(y => y == x));
+int similarityScore = sortedLists.Item1.Sum(x => int.Parse(x) * sortedLists.Item2.Count(y => y == x));
 
 Console.Write(similarityScore.ToString());
---
+++ Day2/Day2.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Immutable;
+
+IEnumerable<string> input = File.ReadLines("./input.txt");
+
+var reports = input.Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(level => int.Parse(level))).ToList();
+Func<int, bool> acceptableRange = x => Math.Abs(x) >= 1 && Math.Abs(x) <= 3;
+
+int safeReports = 0;
+int safeDampenedReports = 0;
+foreach (List<int> report in reports.Select(r => r.ToList()))
+{
+    int totalCells = report.Count - 1;
+    int validCells = GetSafeCellCount(report);
+
+    if (validCells == totalCells)
+    {
+        safeReports++;
+    }
+}
+
+foreach (ImmutableList<int> report in reports.Select(r => r.ToImmutableList()))
+{
+    int totalCells = report.Count - 1;
+    int maxValidCells = report.Select((_, i) => GetSafeCellCount([.. report.RemoveAt(i)])).Max();
+
+    if (maxValidCells >= totalCells - 1)
+    {
+        safeDampenedReports++;
+    }
+}
+
+Console.WriteLine(safeReports);
+Console.WriteLine(safeDampenedReports);
+
+static int GetSafeCellCount(List<int> report)
+{
+    int validCells = 0;
+    for (int i = 1; i < report.Count; i++)
+    {
+        int diff = report[i] - report[i - 1];
+        if (Math.Abs(diff) >= 1 && Math.Abs(diff) <= 3)
+        {
+            if (i > 1)
+            {
+                if ((report[i - 1] - report[i - 2]) * diff > 0)
+                {
+                    validCells++;
+                }
+            }
+            else
+            {
+                validCells++;
+            }
+        }
+    }
+    return validCells;
+}
