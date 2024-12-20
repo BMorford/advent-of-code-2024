15eeed561641eb4fe527999adf29cfcc5e70e2a3
BMorford <22627049+BMorford@users.noreply.github.com>
2024-12-20 12:35:57 -0700

    Add day 3 part 1 solution

---
+++ Day 3/Day3.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+string input = File.ReadAllText("./input.txt");
+string pattern = @"mul\((\d{1,3},\d{1,3})\)";
+Regex regex = new(pattern);
+IEnumerable<string> matches = regex.Matches(input).Select(m => m.Groups[1].Value);
+
+int sum = matches.Sum(m =>
+{
+    int[] operands = [.. m.Split(",").Select(v => int.Parse(v))];
+    return operands[0] * operands[1];
+});
+
+Console.WriteLine(sum.ToString());
