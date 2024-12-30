eaf55816512a024d4783ea002c36f721069600ea
BMorford <22627049+BMorford@users.noreply.github.com>
2024-12-30 12:48:15 -0700

    Add day 4 part 2 solution

--- Day 4/Day4.cs
+++ Day 4/Day4.cs
@@ -45,3 +45,29 @@ bool Search(char[] searchWord, (int, int) direction, int xPos, int yPos)
 }
 
 Console.WriteLine(sum);
+
+// Part 2
+char[] crossSearchWord = [.. "MAS"];
+var aCoords = new List<(int, int)>();
+(int, int)[] crossDirections = [(-1, -1), (-1, 1), (1, -1), (1, 1)];
+for (int x = 0; x < input.Length; x++)
+{
+    for (int y = 0; y < input[x].Length; y++)
+    {
+        if (input[x][y] == crossSearchWord[0])
+        {
+            foreach ((int, int) dir in crossDirections)
+            {
+                // Skip the first letter since we already matched it.
+                if (Search(crossSearchWord[1..], dir, x, y))
+                {
+                    aCoords.Add((x + dir.Item1, y + dir.Item2));
+                }
+            }
+        }
+    }
+}
+int part2Sum = aCoords.GroupBy(coord => (coord.Item1, coord.Item2))
+    .Where(coord => coord.Count() > 1)
+    .Count();
+Console.WriteLine(part2Sum);
