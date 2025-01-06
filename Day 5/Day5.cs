598b87f9425bd1ae5d447ba18b1fcd1423c925b5
BMorford <22627049+BMorford@users.noreply.github.com>
2025-01-06 12:26:45 -0700

    Add day 5 part 2 solution

--- Day 5/Day5.cs
+++ Day 5/Day5.cs
@@ -1,4 +1,6 @@
-﻿string[] input = File.ReadAllLines("./input.txt");
+﻿using System.Collections.ObjectModel;
+
+string[] input = File.ReadAllLines("./input.txt");
 List<IEnumerable<int>> rules = input.Where(r => r.Contains('|')).Select(r => r.Split('|').Select(int.Parse)).ToList();
 List<List<int>> updates = input.Where(r => r.Contains(',')).Select(r => r.Split(",").Select(int.Parse).ToList()).ToList();
 List<List<int>> validUpdates = updates.Where(IsValidUpdate).ToList();
@@ -33,3 +35,52 @@ bool IsValidUpdate(List<int> update)
 
 int sum = validUpdates.Sum(u => u[u.Count / 2]);
 Console.WriteLine(sum);
+
+// Part 2.
+List<Update> invalidUpdates = updates.Where(u => !IsValidUpdate(u)).Select(u => new Update(u, rules)).ToList();
+foreach (Update update in invalidUpdates)
+{
+    update.Pages.Sort();
+}
+
+int correctedSum = invalidUpdates.Sum(u => u.Pages[u.Pages.Count / 2].Value);
+Console.WriteLine(correctedSum);
+
+public class Update
+{
+    public readonly List<Page> Pages;
+    public readonly List<IEnumerable<int>> Rules;
+
+    public Update(List<int> updateValues, List<IEnumerable<int>> rules)
+    {
+        Pages = updateValues.Select(u => new Page(this, u)).ToList();
+        Rules = rules;
+    }
+}
+
+public class Page(Update update, int pageValue) : IComparable
+{
+    public readonly int Value = pageValue;
+    private Update Update = update;
+
+    public int CompareTo(object? obj)
+    {
+        if (obj == null)
+        {
+            return 1;
+        }
+        Page comparePage = obj as Page;
+        int[] applicableRule = Update.Rules.FirstOrDefault(r => r.Contains(Value) && r.Contains(comparePage.Value)).ToArray();
+
+        if (applicableRule == null)
+        {
+            return 0;
+        }
+
+        if (applicableRule[0] == Value)
+        {
+            return -1;
+        }
+        return 1;
+    }
+}
